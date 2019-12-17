f407a81648e68b996adf1ccc075fd516ecf01d6f
Maegan Wilson <maegan@maeganwilson.com>
2019-12-17 15:17:28 -0600

    Adding project

---
+++ TaskList/TaskList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TaskList
+//
+//  Created by Maegan Wilson on 12/17/19.
+//  Copyright © 2019 Maegan Wilson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
