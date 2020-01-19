29546eef79f1f7b5280754176454c0e3474807ba
Maegan Wilson <maegan@maeganwilson.com>
2020-01-19 14:23:43 -0600

    Adjust indention

--- TaskList/TaskList/ContentView.swift
+++ TaskList/TaskList/ContentView.swift
@@ -10,78 +10,78 @@ import SwiftUI
 import CoreData
 
 struct TaskRow: View {
-    var task: Task
-    
-    var body: some View {
-        Text(task.name ?? "No name given")
-    }
+  var task: Task
+  
+  var body: some View {
+    Text(task.name ?? "No name given")
+  }
 }
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) var context
-    
-    @FetchRequest(
-        entity: Task.entity(),
-        sortDescriptors: [NSSortDescriptor(keyPath: \Task.dateAdded, ascending: false)],
-        predicate: NSPredicate(format: "isComplete == %@", NSNumber(value: false))
-    ) var notCompletedTasks: FetchedResults<Task>
-    
-    @State private var taskName: String = ""
-    
-    var body: some View {
-        VStack {
-            HStack{
-                TextField("Task Name", text: $taskName)
-                Button(action: {
-                    self.addTask()
-                }){
-                    Text("Add Task")
-                }
-            }.padding(.all)
-            List {
-                ForEach(notCompletedTasks){ task in
-                    Button(action: {
-                        self.updateTask(task)
-                    }){
-                        TaskRow(task: task)
-                    }
-                }
-            }
+  @Environment(\.managedObjectContext) var context
+  
+  @FetchRequest(
+    entity: Task.entity(),
+    sortDescriptors: [NSSortDescriptor(keyPath: \Task.dateAdded, ascending: false)],
+    predicate: NSPredicate(format: "isComplete == %@", NSNumber(value: false))
+  ) var notCompletedTasks: FetchedResults<Task>
+  
+  @State private var taskName: String = ""
+  
+  var body: some View {
+    VStack {
+      HStack{
+        TextField("Task Name", text: $taskName)
+        Button(action: {
+          self.addTask()
+        }){
+          Text("Add Task")
         }
-    }
-    
-    func addTask() {
-        let newTask = Task(context: context)
-        newTask.id = UUID()
-        newTask.isComplete = false
-        newTask.name = taskName
-        newTask.dateAdded = Date()
-        
-        do {
-            try context.save()
-        } catch {
-            print(error)
+      }.padding(.all)
+      List {
+        ForEach(notCompletedTasks){ task in
+          Button(action: {
+            self.updateTask(task)
+          }){
+            TaskRow(task: task)
+          }
         }
+      }
     }
+  }
+  
+  func addTask() {
+    let newTask = Task(context: context)
+    newTask.id = UUID()
+    newTask.isComplete = false
+    newTask.name = taskName
+    newTask.dateAdded = Date()
     
-    func updateTask(_ task: Task){
-        let isComplete = true
-        let taskID = task.id! as NSUUID
-        let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: "Task")
-        fetchRequest.predicate = NSPredicate(format: "id == %@", taskID as CVarArg)
-        fetchRequest.fetchLimit = 1
-        do {
-            let test = try context.fetch(fetchRequest)
-            let taskUpdate = test[0] as! NSManagedObject
-            taskUpdate.setValue(isComplete, forKey: "isComplete")
-        } catch {
-            print(error)
-        }
+    do {
+      try context.save()
+    } catch {
+      print(error)
     }
+  }
+  
+  func updateTask(_ task: Task){
+    let isComplete = true
+    let taskID = task.id! as NSUUID
+    let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: "Task")
+    fetchRequest.predicate = NSPredicate(format: "id == %@", taskID as CVarArg)
+    fetchRequest.fetchLimit = 1
+    do {
+      let test = try context.fetch(fetchRequest)
+      let taskUpdate = test[0] as! NSManagedObject
+      taskUpdate.setValue(isComplete, forKey: "isComplete")
+    } catch {
+      print(error)
+    }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
